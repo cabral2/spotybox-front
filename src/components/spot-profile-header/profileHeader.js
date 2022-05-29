42496953695e1e1ae4cd9d12fba2dede35de2066
Davi Emediato <48991554+daviemediato@users.noreply.github.com>
2022-05-29 13:46:53 -0300

    profile header component and icons

--- src/components/spot-profile-header/profileHeader.js
+++ src/components/spot-profile-header/profileHeader.js
@@ -1,5 +1,6 @@
 import { CardMedia, Typography } from '@mui/material';
 import { makeStyles } from '@mui/styles';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 
 const useStyles = makeStyles({
     body: {
@@ -35,6 +36,8 @@ const useStyles = makeStyles({
         flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
+        width: '5rem',
+        height: '5rem',
         border: '3px solid #e6e6e6',
         borderRadius: '7px',
         padding: '5px',
@@ -46,6 +49,27 @@ const useStyles = makeStyles({
         borderRadius: '10rem',
         border: '3px solid #ffff',
     },
+    bio: {
+        fontFamily: 'Roboto',
+        fontStyle: 'normal',
+        fontWeight: '500',
+        fontSize: '1rem',
+        lineHeight: '33px',
+    },
+    city: {
+        fontFamily: 'Roboto',
+        fontStyle: 'normal',
+        fontWeight: '300',
+    },
+    cityRow: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        alignItems: 'end',
+        marginTop: '-1rem',
+        gap: '0.3rem',
+        marginLeft: '-0.3rem'
+    }
 });
 
 export default function ProfileHeader({ image, name, bio, city, numFollowers, numFollowing, numAlbums, numReviews }) {
@@ -60,13 +84,16 @@ export default function ProfileHeader({ image, name, bio, city, numFollowers, nu
             />
             <div className={classes.infoSection} >
                 <Typography color="secondary" variant="h5">{name}</Typography>
-                <Typography color="primary" variant="body1">{bio}</Typography>
-                <Typography marginTop="-1rem" color="primary" variant="body1">{city}</Typography>
+                <Typography className={classes.bio} color="secondary" variant="body1">{bio}</Typography>
+                <div className={classes.cityRow}>
+                    <LocationOnIcon color="secondary" />
+                    <Typography className={classes.city} marginTop="-1rem" color="secondary" variant="body1">{city}</Typography>
+                </div>
                 <div className={classes.infoFollower}>
-                    <Typography color="primary" variant="body1">{numFollowers}</Typography>
-                    <Typography marginLeft="-0.8rem" color="primary" variant="body1">Followers</Typography>
-                    <Typography color="primary" variant="body1">{numFollowing}</Typography>
-                    <Typography marginLeft="-0.8rem" color="primary" variant="body1">Following</Typography>
+                    <Typography color="secondary" variant="body1">{numFollowers}</Typography>
+                    <Typography marginLeft="-0.8rem" color="secondary" variant="body1">Followers</Typography>
+                    <Typography color="secondary" variant="body1">{numFollowing}</Typography>
+                    <Typography marginLeft="-0.8rem" color="secondary" variant="body1">Following</Typography>
                 </div>
             </div>
             <div className={classes.sectionNumber}>
