8ec5b12fe8a7318db0935a5d815f513e66be45c6
cabral2 <saviocampolina9@gmail.com>
2022-05-30 20:24:49 -0300

    Add followers/following tab

--- src/components/user-card/index.js
+++ src/components/user-card/index.js
@@ -1,48 +1,56 @@
 import React from "react";
-import Image from 'next/image'
+import Image from "next/image";
 
-import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import CardMedia from "@mui/material/CardMedia";
 import { makeStyles } from "@mui/styles";
 
-import defaultProfile from '../../assets/pictures/profile-1.jpg';
+import defaultProfile from "../../assets/pictures/profile-1.jpg";
 import ButtonSpoty from "../button";
+import { height } from "@mui/system";
 
-const useStyles = makeStyles({
-  box:{
-    width: 700
-  },
-  picture:{
-    borderRadius: 50
-  },
-  text:{
-    color: 'white',
-  },
-  name: {
-    color: 'white',
-    fontSize: 22,
-  },
-  description:{
-    color: '#7D7D7D',
-    fontSize: 18,
-  }
+const useStyles = makeStyles((theme) => {
+  return {
+    box: {
+      width: "100%",
+      marginTop: "1.5rem",
+    },
+    picture: {
+      borderRadius: "50%",
+      width: "5rem",
+      height: "5rem",
+    },
+    text: {
+      color: theme?.palette?.primary?.contrastText,
+    },
+    name: {
+      color: theme?.palette?.primary?.contrastText,
+    },
+    description: {
+      color: theme?.palette?.subtitle?.color,
+    },
+  };
 });
-
 
 export default function UserCard(props) {
   const styles = useStyles();
   const imageUrl = props.imageUrl || defaultProfile;
 
   let type = null;
-  if (props.unfollow) type = {unfollow: true}
-  if (props.blockUser) type = {blockUser: true}
+  if (props.unfollow) type = { unfollow: true };
+  if (props.blockUser) type = { blockUser: true };
 
-  return(
+  return (
     <Box className={styles.box}>
       <Grid container alignItems="center" spacing={1}>
         <Grid item xs={2}>
-          <Image className={styles.picture} src={imageUrl} alt="Image profile" width={100} height={100}/>
+          <CardMedia
+            className={styles.picture}
+            component="img"
+            image={imageUrl}
+          />
         </Grid>
         <Grid item xs={6}>
           <Typography className={styles.text}>
@@ -55,8 +63,8 @@ export default function UserCard(props) {
           </Typography>
         </Grid>
         <Grid item xs={4}>
-          <ButtonSpoty {...type}/>
-        </Grid>   
+          <ButtonSpoty {...type} />
+        </Grid>
       </Grid>
     </Box>
   );
