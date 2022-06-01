895a340271f549c0ec50b7532cba49e894a6b98b
cabral2 <saviocampolina9@gmail.com>
2022-05-31 21:04:37 -0300

    Fix colors on card

--- src/components/spot-album-card/albumCard.js
+++ src/components/spot-album-card/albumCard.js
@@ -1,59 +1,61 @@
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { makeStyles } from '@mui/styles';
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { makeStyles } from "@mui/styles";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => {
+  return {
     cardContainer: {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'flex-start',
-        justifyContent: 'center',
-        width: '16rem',
-        height: '16rem',
-        borderRadius: '0.7rem',
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "flex-start",
+      justifyContent: "center",
+      width: "16rem",
+      height: "16rem",
+      borderRadius: "0.7rem",
+      backgroundColor: theme?.palette?.primary?.main,
     },
     cardMedia: {
-        height: '10rem',
-        width: '100%',
+      height: "10rem",
+      width: "100%",
     },
     cardContent: {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'flex-start',
-        width: '100%',
-        height: '10rem',
-        overflow: 'hidden',
-    }
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "flex-start",
+      width: "100%",
+      height: "10rem",
+      overflow: "hidden",
+    },
+    subtitle: {
+      color: theme?.palette?.subtitle?.color,
+    },
+  };
 });
 
 export default function AlbumCard({ title, albumName, image }) {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-        <Card className={classes.cardContainer}>
-            <CardMedia
-                className={classes.cardMedia}
-                component="img"
-                image={image}
-            />
-            <CardContent className={classes.cardContent}>
-                <Typography
-                    color="secondary"
-                    letterSpacing="0.1rem"
-                    className="text-primary"
-                >
-                    {title}
-                </Typography>
-                <Typography
-                    color="secondary"
-                    letterSpacing="0.1rem"
-                    className="text-secondary"
-                >
-                    {albumName}
-                </Typography>
-            </CardContent>
-        </Card>
-    );
+  return (
+    <Card className={classes.cardContainer}>
+      <CardMedia className={classes.cardMedia} component="img" image={image} />
+      <CardContent className={classes.cardContent}>
+        <Typography
+          color="secondary"
+          letterSpacing="0.1rem"
+          //   className="text-primary"
+        >
+          {title}
+        </Typography>
+        <Typography
+          color="secondary"
+          letterSpacing="0.1rem"
+          className={classes.subtitle}
+        >
+          {albumName}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
 }
